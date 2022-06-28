cb1cb05a8509b39af6cc46f6e00eec5761de3730
ducksplash <11778864+ducksplash@users.noreply.github.com>
2022-06-28 18:58:34 +0100

    28/06/22

---
+++ Assets/Scripts/clock.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class clock : MonoBehaviour
+{
+    public TextMeshProUGUI timereadout;
+
+
+
+    void LateUpdate()
+    {
+        DateTime nowDateTime = DateTime.Now;
+        string anHour = nowDateTime.Hour.ToString().PadLeft(2, '0');
+        string aMinute = nowDateTime.Minute.ToString().PadLeft(2, '0');
+
+
+        timereadout.text =  anHour + ":" + aMinute;
+        
+    }
+}

3fc1dad40420ea55f402e4240c29bcd1cb1b6510
ducksplash <11778864+ducksplash@users.noreply.github.com>
2022-07-04 15:08:23 +0100

    04/07/22
    Dialogue Framework

--- Assets/Scripts/DialogueBeef.cs
+++ Assets/Scripts/DialogueBeef.cs
@@ -11,101 +11,39 @@ using UnityEngine.EventSystems;
 public class DialogueBeef : MonoBehaviour
 {
 
-    public bool AlreadyTriggered;
+    [Header("Message from:")]
+    public string ContactName;
+    [Header("Message contents:")]
+    [TextArea(5, 10)]
+    public string MessageBody;
+
+    [Header("Display time in seconds")]
+    public float DisplayTimer = 5.0f;
+
+
+    [Header("Debug (ignore)")]
     public float forhowlong;
-
-
-    void Start()
-    {
-
-    }
-
-
-
-
-
-
 
 
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer == 3 && !AlreadyTriggered)
+        if (other.gameObject.layer == 3)
         {
 
-            AlreadyTriggered = true;
-
-            // are you ready?
-
-            if (gameObject.name.ToLower().Contains("areyouready".ToLower()))
-            {
-                var TheCallingObject = gameObject;
-
-                var TheContact = "Kieron";
-                var TheMessage = "Before you head out, are you sure you've everything you need?";
-                var Timer = 5;
-
-                forhowlong = Timer;
-
-                other.gameObject.GetComponent<DialogueManager>().NewDialogue(TheContact, TheMessage, Timer, TheCallingObject);
-            }
+            gameObject.GetComponent<Collider>().enabled = false;
 
 
+            var TheCallingObject = gameObject;
+            forhowlong = DisplayTimer;
 
-
-
-            // Manager's office: turn on the power
-
-            if (gameObject.name.ToLower().Contains("managersoffice".ToLower()))
-            {
-                var TheCallingObject = gameObject;
-
-                var TheContact = "Kieron";
-                var TheMessage = "This looks like the manager's office.  That computer probably controls the doors, but you'll need to get down to the basement and turn on the power first.";
-                var Timer = 10;
-
-                forhowlong = Timer;
-
-                other.gameObject.GetComponent<DialogueManager>().NewDialogue(TheContact, TheMessage, Timer, TheCallingObject);
-            }
-
-
-
-
-
-            // by the power room door
-
-            if (gameObject.name.ToLower().Contains("powerroom".ToLower()))
-            {
-                var TheCallingObject = gameObject;
-
-                var TheContact = "Kieron";
-                var TheMessage = "The power room; the door seems to be locked... I don't think there's gonna be a key around here, maybe try and bash it with your torch?  I know it's a long shot.";
-                var Timer = 10;
-
-
-                forhowlong = Timer;
-
-
-                other.gameObject.GetComponent<DialogueManager>().NewDialogue(TheContact, TheMessage, Timer, TheCallingObject);
-            }
-
-            StartCoroutine(UnTrigger());
+            other.gameObject.GetComponent<DialogueManager>().NewDialogue(ContactName, MessageBody, DisplayTimer, TheCallingObject);
+           
 
         }
     }
 
-    IEnumerator UnTrigger()
-    {
 
-        var TimeToWait = forhowlong + 0.5f;
-
-        yield return new WaitForSeconds(TimeToWait);
-
-        AlreadyTriggered = false;
-
-
-    }
 
 
 
--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -13,8 +13,8 @@ public class DialogueManager : MonoBehaviour
     public GameObject DialogManager;
     public CanvasGroup DialogManagerCanvas;
     public TextMeshProUGUI ContactName;
+    public Image ContactPhoto;
     public TextMeshProUGUI ReceivedMessage;
-
     public bool DialogInProgress;
     public Image timebar;
     public float messagetimer = 0f;
@@ -37,18 +37,38 @@ public class DialogueManager : MonoBehaviour
 
     public void NewDialogue(string Contact, string message, float displaytimer, GameObject CallingObject)
     {
+
+        if (DialogInProgress)
+        {
+            StartCoroutine(Queuer(Contact, message, displaytimer, CallingObject));
+        }
+        else
+        {
+            CreateDialogue(Contact, message, displaytimer, CallingObject);
+        }
+
+    }
+
+
+    public IEnumerator Queuer(string Contact, string message, float displaytimer, GameObject CallingObject)
+    {
+        yield return new WaitForSeconds(0.5f);
+        NewDialogue(Contact, message, displaytimer, CallingObject);
+    }
+
+
+
+    public void CreateDialogue(string Contact, string message, float displaytimer, GameObject CallingObject)
+    {
         DialogInProgress = true;
         messagetimer = displaytimer;
 
-        StartCoroutine(Fader(DialogManagerCanvas, 1));
 
 
         ContactName.text = Contact;
         ReceivedMessage.text = message;
 
         StartCoroutine(MessageTimer(displaytimer, CallingObject));
-
-
     }
 
 
@@ -56,31 +76,51 @@ public class DialogueManager : MonoBehaviour
     public IEnumerator Fader(CanvasGroup ThisCanvas, int direction)
     {
         var counter = 9;
-
-        while (counter > 0)
+        if (direction == 0)
         {
 
-            yield return new WaitForSeconds(0.05f);
-
-            if (direction == 0)
+            while (counter > 0)
             {
+
+                yield return new WaitForSeconds(0.05f);
+
                 ThisCanvas.alpha -= 0.1f;
-            }
-            else
-            {
-                ThisCanvas.alpha += 0.1f;
+
+                counter--;
+
+
             }
 
-            counter--;
         }
+        else
+        {
 
+            while (counter > 0)
+            {
+
+                yield return new WaitForSeconds(0.05f);
+
+
+                ThisCanvas.alpha += 0.1f;
+
+                counter--;
+
+            }
+
+        }
     }
+
+
+
 
 
 
 
     public IEnumerator MessageTimer(float timevalue, GameObject CalledBy)
     {
+        timebar.fillAmount = 1.0f;
+        StartCoroutine(Fader(DialogManagerCanvas, 1));
+
         yield return new WaitForSeconds(timevalue);
 
         StartCoroutine(Fader(DialogManagerCanvas, 0));

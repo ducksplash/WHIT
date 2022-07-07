8f60a13455ac5c56e2d5c303f9d42d37ee8986b9
ducksplash <11778864+ducksplash@users.noreply.github.com>
2022-07-07 13:04:28 +0100

    07/07/22

---
+++ Assets/Scripts/solidobject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class solidobject : MonoBehaviour
+{
+
+    public Transform defaultparent;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.layer == 0 || collision.gameObject.layer == 6 || collision.gameObject.layer == 7 || collision.gameObject.layer == 8)
+        {
+
+            transform.SetParent(defaultparent);
+            transform.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            pickup.hasobject = false;
+            GameMaster.HASITEM = false;
+            GameObject.FindGameObjectWithTag("ROTATIONMENU").GetComponent<CanvasGroup>().alpha = 0;
+                        
+        }
+    }
+
+
+}

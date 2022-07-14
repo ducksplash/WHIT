a27b05e251f7512ab61608f876354dd2254a246a
ducksplash <11778864+ducksplash@users.noreply.github.com>
2022-07-15 00:13:59 +0100

    15/07/22

---
+++ Assets/Prefabs/Factory/WarningLight/WarningLight.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarningLight : MonoBehaviour
+{
+
+	public float lightspeed = 300;
+
+    void FixedUpdate()
+	{
+		gameObject.transform.Rotate(new Vector3(0, 0, 1) * (Time.fixedDeltaTime * lightspeed));
+	}
+}

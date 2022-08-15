50e6afab3637bb7913e964921c3ad3ddf0bb07c6
ducksplash <11778864+ducksplash@users.noreply.github.com>
2022-08-15 18:00:09 +0100

    15/08/2022

---
+++ Assets/Scripts/MenuButtonMouseOver.cs
@@ -0,0 +1,43 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MenuButtonMouseOver : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    private TextMeshProUGUI theText;
+    public Color OrigColor;
+    public Color HoverColor = Color.white;
+
+    public Material normalMaterial;
+    public Material hoverMaterial;
+
+
+    public void Start()
+    {
+        theText = gameObject.GetComponent<TextMeshProUGUI>();
+
+        OrigColor = theText.color;
+        theText.fontSharedMaterial = normalMaterial;
+    }
+
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        theText.color = HoverColor;
+        //theText.fontStyle = FontStyles.Underline;
+
+        theText.fontSharedMaterial = hoverMaterial;
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        theText.color = OrigColor;
+        theText.fontStyle = FontStyles.Normal;
+        theText.fontSharedMaterial = normalMaterial;
+    }
+}
+
+
+

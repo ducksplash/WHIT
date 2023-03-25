6bda345cc34234c1808b6ebffef450943ccf85e5
Thomas Kane <davelister00169@gmail.com>
2023-03-25 23:55:12 +0000

    lift controller

--- Assets/Scripts/LiftController.cs
+++ Assets/Scripts/LiftController.cs
@@ -10,6 +10,15 @@ public class LiftController : MonoBehaviour
     public Animator outer_door_animator;
     public string currentfloor = "G";
     public bool liftismoving;
+    public bool doorsmoving;
+
+    public Transform FloorStopG;
+    public Transform FloorStop1;
+    public Transform FloorStopB1;
+    public Transform FloorStopB2;
+    public Transform CARRIAGE;
+
+    public GameObject Player;
 
 
     private void Update()
@@ -20,10 +29,17 @@ public class LiftController : MonoBehaviour
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
-                if (hit.distance <= 5.5f)
+                if (hit.transform.name == "X" || hit.transform.name == "G" || hit.transform.name == "1" || hit.transform.name == "B1" || hit.transform.name == "B2")
                 {
-                    CallTheLift(hit.transform.name);
+                    if (hit.distance <= 2.5f)
+                    {
+                        if (!liftismoving && !doorsmoving)
+                        {
+                            CallTheLift(hit.transform.name);
+                        }
+                    }
                 }
+
             }
         }
     }
@@ -33,33 +49,31 @@ public class LiftController : MonoBehaviour
     public void CallTheLift(string calledfrom)
     {
 
-        if (!liftismoving)
-        {
-            if (calledfrom == "X" || calledfrom == currentfloor)
-            {
-                if (!isDoorOpen)
-                {
-                    OpenLiftDoors();
-                    Debug.Log("lift called from outside [X] or same as current floor");
-                } 
-            }
-            else
-            {
-                GoToFloor(calledfrom);
 
-            }
+        if (calledfrom == "X")
+        {
+            if (!isDoorOpen)
+            {
+                OpenLiftDoors();
+            } 
         }
+        else
+        {
+            StartCoroutine(GoToFloor(calledfrom));
+        }
+        
     }
 
 
 
     public void OpenLiftDoors()
     {
+        doorsmoving = true;
         inner_door_animator.Play("opened");     
         outer_door_animator.Play("opened");     
         isDoorOpen = true;
+        doorsmoving = false;
     }
-
 
 
 
@@ -76,25 +90,82 @@ public class LiftController : MonoBehaviour
 
         if (isDoorOpen)
         {
+            doorsmoving = true;
             inner_door_animator.Play("closed");
             outer_door_animator.Play("closed");
             isDoorOpen = false;
+            doorsmoving = false;
         }
     }
 
 
 
 
-    public void GoToFloor(string floorselected)
-    {
-        
-        //liftismoving = true;
-        
-        Debug.Log("lift called from inside, "+floorselected+" floor");
-        Debug.Log(floorselected);
 
+
+    public IEnumerator GoToFloor(string floorselected)
+    {
+
+        GameMaster.FROZEN = true;
+        
+        // close doors
+        yield return StartCoroutine(CloseLiftDoors());
+
+        // move lift
+        liftismoving = true;
+
+
+
+        Player.transform.SetParent(CARRIAGE);
+        Vector3 startPosition = CARRIAGE.position;
+        Vector3 targetPos = CARRIAGE.position;
+        
+        if (floorselected == "G")
+        {
+            targetPos.y = FloorStopG.position.y;
+        }
+        else if (floorselected == "1")
+        {
+            targetPos.y = FloorStop1.position.y;
+        }
+        else if (floorselected == "B1")
+        {
+            targetPos.y = FloorStopB1.position.y;
+        }
+        else if (floorselected == "B2")
+        {
+            targetPos.y = FloorStopB2.position.y;
+        }
+
+        float startTime = Time.time;
+        float duration = 5.0f;
+
+        while (Time.time - startTime < duration)
+        {
+            float timeFraction = (Time.time - startTime) / duration;
+            CARRIAGE.position = Vector3.Lerp(startPosition, targetPos, timeFraction);
+            yield return null;
+        }
+
+        CARRIAGE.position = targetPos;
+
+        currentfloor = floorselected;
+
+        Debug.Log("lift called from inside, "+floorselected+" floor");            
+        Player.transform.SetParent(null);
+
+        liftismoving = false;
+
+        // open doors
+        OpenLiftDoors();
+
+
+        GameMaster.FROZEN = false;
 
     }
+
+
+
 
 
     private void OnTriggerExit(Collider other)
